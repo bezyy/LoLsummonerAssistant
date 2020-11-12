cc514c5985ae65aa1d11709e25cb1a9c3ca5d97e
Ostap Bezrukyy <byostap@gmail.com>
2020-11-11 20:43:44 -0500

    Update main.py
    recognize key words and return output

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pynput
 import pyperclip
 import os
 import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+from pynput import keyboard
 
 # says "done" when summoner spell timer is copied to clipboard
 def speak():
@@ -24,9 +26,12 @@ def get_audio():
 
 	return said
 
+#determines which mode user wants to use
+mode = input("Enter 0 for voiceRecognition mode OR enter 1 for textRecognition mode: ")
+
 # this while loop makes it so the program is constantly listening
 # here it searches for specific key words like the lane and type of spell
-while True:
+while int(mode) == 0:
 	text = get_audio()
 
 	if "top" in text:
@@ -97,4 +102,41 @@ while True:
 			speak()
 		if "barrier" in text or "Barrier" in text:
 			pyperclip.copy("champnamesupport barrier time")
-			speak()
+			speak
+
+keys = ["a", "b", "c", "d"]
+count = 0
+while int(mode) == 1:
+	def on_press(key):
+		global keys, count
+		if count < 4:
+			del keys[int(count)]
+			keys.insert(int(count), str(key))
+			count += 1
+		else:
+			count = 0
+			del keys[int(count)]
+			keys.insert(int(count), str(key))
+			count += 1
+
+	def on_release(key):
+		return False
+	
+	with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+		listener.join()
+
+	if "'t'" in keys and "'o'" in keys and "'p'" in keys:
+		if "'f'" in keys:
+			pyperclip.copy("champnameTOP flash time")
+	if "'j'" in keys and "'u'" in keys and "'n'" in keys:
+		if "'f'" in keys:
+			pyperclip.copy("champnameJungle flash time")
+	if "'m'" in keys and "'i'" in keys and "'d'" in keys:
+		if "'f'" in keys:
+			pyperclip.copy("champnameMID flash time")
+	if "'a'" in keys and "'d'" in keys and "'c'" in keys:
+		if "'f'" in keys:
+			pyperclip.copy("champnameADC flash time")
+	if "'s'" in keys and "'u'" in keys and "'p'" in keys:
+		if "'f'" in keys:
+			pyperclip.copy("champnameSUP flash time")
